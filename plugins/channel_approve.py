2f1f14499132f6c7a6300d1a8e27d3682a1b3d9f
Albin Binu <93376762+Albin-Binu@users.noreply.github.com>
2023-01-31 12:32:33 +0530

    Syn

--- plugins/channel_approve.py
+++ plugins/channel_approve.py
@@ -32,9 +32,9 @@ async def autoapprove(c, m):
         reply_markup=markup
     )
 
-    except UserIsBlocked:
-        print(f"{m.from_user.first_name} blocked the bot")
-    except PeerIdInvalid:
-        print(f"User {m.from_user.first_name} haven't started the bot yet")
-    except Exception as e:
-        print('Error:', e)
+        except UserIsBlocked:
+            print(f"{m.from_user.first_name} blocked the bot")
+        except PeerIdInvalid:
+            print(f"User {m.from_user.first_name} haven't started the bot yet")
+        except Exception as e:
+            print('Error:', e)
