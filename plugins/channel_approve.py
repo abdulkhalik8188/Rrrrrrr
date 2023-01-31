5163bf781cc74b649bd5aac4a51947b406f4b85a
Albin Binu <93376762+Albin-Binu@users.noreply.github.com>
2023-01-31 11:51:28 +0530

    Update channel_approve.py

--- plugins/channel_approve.py
+++ plugins/channel_approve.py
@@ -13,5 +13,27 @@ from pyrogram import Client as bin, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
 
+#function
 
-
+@bin.on_chat_join_request(filters.group | filters.channel)
+async def autoapprove(c, m):
+    await c.approve_chat_join_request(m.chat.id, m.from_user.id)
+        button = [[
+            InlineKeyboardButton('🎬JOIN MOVIE CHANNEL🎬', url='https://t.me/at3moviesofficalbot')
+            ],[
+            InlineKeyboardButton('📽️JOIN MOVIE GROUP📽️', url='https://t.me/MovieGroup_TM')
+        ]]
+        markup = InlineKeyboardMarkup(button)
+        caption = f'Hello {m.from_user.mention()}\nYou Request To Join {m.chat.title} Was Approved.'
+        await c.send_photo(
+            m.from_user.id, 
+            photo='https://telegra.ph/file/f7738f04ea74e16c9db02.jpg', 
+            caption=caption, 
+            reply_markup=markup
+        )
+    except UserIsBlocked:
+        print(f"{m.from_user.first_name} blocked the bot")
+    except PeerIdInvalid:
+        print(f"User {m.from_user.first_name} haven't started the bot yet")
+    except Exception as e:
+        print('Error:', e)
