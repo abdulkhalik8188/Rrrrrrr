050e6defacb584fb9224ac0c2ab57cb0c8f03e00
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-01-31 23:44:10 +0530

    Update song.py

--- plugins/helpers/song.py
+++ plugins/helpers/song.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ›› [🍁 ᴄʜᴀɴɴᴇʟ 🍁](https://t.me/czdbotz)**\n**𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 ›› [muѕíc](https://t.me/music_playboybot)**\n@𝙼𝚘𝚟𝚒𝚎 𝙵𝚕𝚒𝚡**'
+        rep = '**𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 ›› [🍁 ᴄʜᴀɴɴᴇʟ 🍁](https://t.me/czdbotz)**\n**𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 ›› [muѕíc](https://t.me/music_playboybot)**\n@𝙲𝙸𝙽𝙴𝙼𝙰𝙺𝙾𝚅𝙸𝙻𝙰𝙺𝙰𝙼**'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
