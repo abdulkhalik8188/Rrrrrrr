e494f10cae793fc89b5f581f54c59d1ab4757d33
Albin Binu <93376762+Albin-Binu@users.noreply.github.com>
2023-02-09 00:45:00 +0530

    Update __approve.py

--- plugins/helper/__approve.py
+++ plugins/helper/__approve.py
@@ -3,6 +3,7 @@ import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message, User, ChatJoinRequest
 from info import CHAT_ID, TEXT, APPROVED 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
 async def autoapprove(client, message: ChatJoinRequest):
