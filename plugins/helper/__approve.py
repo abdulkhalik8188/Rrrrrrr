4d310061a79e91eed1368186a36bdee1bef6fe42
Albin Binu <93376762+Albin-Binu@users.noreply.github.com>
2023-02-09 01:15:18 +0530

    Update __approve.py

--- plugins/helper/__approve.py
+++ plugins/helper/__approve.py
@@ -7,7 +7,9 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
 
 
+
 ##@Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
+"""
 @Client.on_chat_join_request(filters.group | filters.channel)
 async def autoapprove(client, message: ChatJoinRequest):
     chat=message.chat 
@@ -29,9 +31,31 @@ async def autoapprove(client, message: ChatJoinRequest):
         )
         except UserIsBlocked:
         print(f"{m.from_user.first_name} blocked the bot")
+        except PeerIdInvalid:
+        print(f"User {m.from_user.first_name} haven't started the bot yet")
+        except Exception as e:
+        print('Error:', e)
+"""
+@Client.on_chat_join_request(filters.group | filters.channel)
+async def autoapprove(c, m):
+    await c.approve_chat_join_request(m.chat.id, m.from_user.id)
+        button = [[
+            InlineKeyboardButton('🎬JOIN MOVIE CHANNEL🎬', url='https://t.me/at3moviesofficalbot')
+            ],[
+            InlineKeyboardButton('📽️JOIN MOVIE GROUP📽️', url='https://t.me/MovieGroup_TM')
+        ]]
+        markup = InlineKeyboardMarkup(button)
+        caption = f'Hello {m.from_user.mention()}\nYou Request To Join {m.chat.title} Was Approved.'
+        await c.send_photo(
+            m.from_user.id, 
+            photo='https://telegra.ph/file/f7738f04ea74e16c9db02.jpg', 
+            caption=caption, 
+            reply_markup=markup
+        )
+    except UserIsBlocked:
+        print(f"{m.from_user.first_name} blocked the bot")
     except PeerIdInvalid:
         print(f"User {m.from_user.first_name} haven't started the bot yet")
     except Exception as e:
         print('Error:', e)
 
-    
