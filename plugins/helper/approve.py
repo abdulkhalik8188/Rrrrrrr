f0ee9cebc1ae2fa94e8271daa499aa07effc6639
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-02-09 06:54:57 +0530

    Update approve.py

--- plugins/helper/approve.py
+++ plugins/helper/approve.py
@@ -12,9 +12,4 @@ async def autoapprove(client, message: ChatJoinRequest):
     print(f"{user.first_name} Joined (Approved)") 
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
     if APPROVED == "on":
-        buttons = [[
-            InlineKeyboardButton('albin', url=f'https://t.me/albin')
-            
-        ]]
-        markup = InlineKeyboardMarkup(buttons)
         await client.send_message(chat_id=chat.id, text=TEXT.format(mention=user.mention, title=chat.title))
