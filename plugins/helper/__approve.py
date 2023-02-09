2678fe86baa7d33b756634b7f1e2ca7c5398be8d
Albin Binu <93376762+Albin-Binu@users.noreply.github.com>
2023-02-09 19:36:01 +0530

    Update __approve.py

--- plugins/helper/__approve.py
+++ plugins/helper/__approve.py
@@ -9,7 +9,7 @@ from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, Peer
 
 
 @Client.on_chat_join_request((filters.group | filters.channel) & filters.chat(CHAT_ID) if CHAT_ID else (filters.group | filters.channel))
-async def autoapprove(client, bot, message: ChatJoinRequest):
+async def autoapprove(client, message: ChatJoinRequest):
     chat=message.chat 
     user=message.from_user 
     print(f"{user.first_name} Joined") 
@@ -20,10 +20,10 @@ async def autoapprove(client, bot, message: ChatJoinRequest):
             
         ]]
         markup = InlineKeyboardMarkup(buttons)
-        caption = f'Hello {message.from_user.mention()}\nYou Request To Join {message.chat.title} Was Approved.'
-        await bot.send_message(
+        msg = f'Hello {message.from_user.mention()}\nYou Request To Join {message.chat.title} Was Approved.'
+        await client.reply_message(
             message.from_user.id,
-            message=caption, 
+            message=msg, 
             reply_markup=markup
         )
          
