e0b85b845d5fee82caa2322a9067c4aa70b226c4
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-02-14 20:59:13 +0530

    Update password.py

--- plugins/helpers/password.py
+++ plugins/helpers/password.py
@@ -12,5 +12,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ', url='https://t.me/MLZ_BOTZ_SUPPORT')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ 🔐', url='https://t.me/MLZ_BOTZ_SUPPORT')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
