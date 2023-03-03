0c3467f5c7a1a33671ff1aebb528e5616f72cec4
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-03-03 19:28:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -101,6 +101,9 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 
 LANGUAGES = ["malayalam", "tamil", "english", "hindi", "telugu", "kannada"]
 
+# Delete Time
+DELETE_TIME = int(environ.get('DELETE_TIME', 60))
+
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
 LOG_STR += ("P_TTI_SHOW_OFF found , Users will be redirected to send /start to Bot PM instead of sending file file directly\n" if P_TTI_SHOW_OFF else "P_TTI_SHOW_OFF is disabled files will be send in PM, instead of sending start.\n")
