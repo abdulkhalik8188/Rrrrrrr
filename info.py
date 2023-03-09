ce55f5ef4dc66c69afbe8547abeb5472971c0240
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-03-09 22:48:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
 
 # Custom Chats
-FILE_CHANNEL = int(environ.get('FILE_CHANNEL', '-1001832732995'))
+FILE_CHANNEL = environ.get('FILE_CHANNEL', '-1001832732995')
 FILE_CHANNEL_LINK = environ.get('FILE_CHANNEL_LINK', 'https://t.me/+e7sbQsiP7tY4N2U1')
 
 #VALUES
