4884731b3324ae8e08953acb9ab98bc4bbb43e6b
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-03-10 23:50:23 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -120,13 +120,11 @@ Cᴏᴍᴍᴀɴᴅs Aɴᴅ Usᴀɢᴇ:
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
 Added By - {}
-{}
 """
 
     LOG_TEXT_P = """#NewUser
 ID - <code>{}</code>
 Nᴀᴍᴇ - {}
-{]
 """
     ALRT_TXT = """ʜᴇʟʟᴏ {},
 ᴛʜɪꜱ ɪꜱ ɴᴏᴛ ʏᴏᴜʀ ᴍᴏᴠɪᴇ ʀᴇQᴜᴇꜱᴛ,
