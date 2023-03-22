3b8c6318ab395a3594fc37b7167696f5305e896d
ᴀꜰsᴀʟ ʜᴀsʜɪᴍ​ <123615887+CyberTG@users.noreply.github.com>
2023-03-22 20:10:51 +0530

    Update openai.py

--- plugins/local_plugins/openai.py
+++ plugins/local_plugins/openai.py
@@ -1,22 +1,34 @@
+from urllib import response
 import openai
-import os
+from pyrogram import Client, filters, enums
+from info import ADMINS
+import datetime
 
-# Authenticate with the API key
-openai.api_key = os.environ["OPENAI_API_KEY"]
+OPEN_AI_KEY = "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"
 
-# Define the prompt and parameters for the text generation
-prompt = "Write a short story about a robot that gains consciousness"
-model = "text-davinci-002"
-temperature = 0.7
-max_tokens = 100
+def ai_responses(input_text):
+    user_message = str(input_text).lower()
+    prompt_addition = "ENTER YOUR PROMPT HERE" + user_message
+    openai.api_key = OPEN_AI_KEY
+    bot_output = openai.Completion.create(engine="text-davinci-002",prompt=prompt_addition,temperature=0.7,max_tokens=200,top_p=1,frequency_penalty=0,presence_penalty=0)
+    response = bot_output['choices'][0]['text']
+    return response
+  
+@Client.on_message(filters.command("openai") & filters.user(ADMINS))
+async def delvarrrssz(bot, message):
+    query = message.text.split(" ", maxsplit=1)[1]
 
-# Generate the text
-response = openai.Completion.create(
-  engine=model,
-  prompt=prompt,
-  temperature=temperature,
-  max_tokens=max_tokens
-)
-
-# Print the generated text
-print(response.choices[0].text)
+    response = ai_responses(query)
+    await message.reply_text(response)
+    
+@Client.on_message(filters.command("get") & filters.user(ADMINS))
+async def delvarrrssz(bot, message):
+    m = datetime.datetime.now
+    time = m.hour
+    if time < 12:
+        get="Good Morning"
+    elif time < 15: 
+        get="Good After Noon"
+    else:
+        get="Good Even"
+    await message.reply_text(f"{get}")
