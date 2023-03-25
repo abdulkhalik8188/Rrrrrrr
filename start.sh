a0da853b779f9aebdffbcd22e7e1fa8b013add20
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-25 21:19:20 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/Azanpopz/New-Tom-last.git /TOM-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TOM-BOT
+  git clone $UPSTREAM_REPO /New-Tom-last
 fi
-cd /TOM-BOT
+cd /New-Tom-last
 pip3 install -U -r requirements.txt
 echo "Starting...."
 python3 bot.py
