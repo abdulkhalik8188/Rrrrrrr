34e7c21a4581f1334d867fe393632fbd292a9588
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2023-03-25 21:30:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Azanpopz/New-Tom-last.git /TOM-BOT
+  git clone https://github.com/Azanpopz/New-Tom-last.git /New-Tom-last
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /New-Tom-last
